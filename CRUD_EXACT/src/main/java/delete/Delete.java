0637b5ac6e718bdac179bd238a9981c7b98e9e2e
ccampos <ccampos@exact.com.pe>
2018-10-09 10:09:47 -0500

    hola a todos

---
+++ CRUD_EXACT/src/main/java/delete/Delete.java
@@ -0,0 +1,43 @@
+package delete;
+
+public class Delete {
+
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+	public Delete(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+
+	
+	public void saludo() {
+		System.out.println("hola a todos");
+	}
+	
+}
