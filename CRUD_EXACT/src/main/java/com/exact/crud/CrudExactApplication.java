a858717cff6f274b6a12bac297aa50079f639f16
e-oheredia <oheredia@exact.com.pe>
2018-10-09 09:34:11 -0500

    Creación del Proyecto

---
+++ CRUD_EXACT/src/main/java/com/exact/crud/CrudExactApplication.java
@@ -0,0 +1,12 @@
+package com.exact.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExactApplication.class, args);
+	}
+}
