256403492754af11726e236fd1659fd5581cdec8
e-oheredia <oheredia@exact.com.pe>
2018-10-09 09:52:56 -0500

    Creación de la Clase Update Service

---
+++ CRUD_EXACT/src/main/java/com/exact/crud/service/UpdateService.java
@@ -0,0 +1,18 @@
+package com.exact.crud.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateService {
+	
+	private final Logger logger = LoggerFactory.getLogger(UpdateService.class);
+	
+	private final String mensaje = "Está actualizando"; 
+		
+	public String update() {
+		logger.info(mensaje);
+		return mensaje;
+	}
+}
