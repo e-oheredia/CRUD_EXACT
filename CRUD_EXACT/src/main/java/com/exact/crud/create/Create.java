67ae71546d86424963694e7249ae05be5f7c8765
RonaldSantosR <rsantos@exact.com.pe>
2018-10-09 09:50:27 -0500

    commit  -CreateEnMasterRemoto

---
+++ CRUD_EXACT/src/main/java/com/exact/crud/create/Create.java
@@ -0,0 +1,38 @@
+package com.exact.crud.create;
+
+public class Create {
+	
+	public int idUsuario;
+	public String Nombre;
+	public String Apellido;
+	public int edad;
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+	
+
+}
